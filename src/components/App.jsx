25d9f64d2b58f77de84667af1e09a6365df4474f
anastasia <anastasiaverbenets@gmail.com>
2025-04-18 17:27:36 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 const { Component } = require('react');
 const { Feedback } = require('./Feedback/Feedback');
 const { Statistics } = require('./Statistics/Statistics');
+const { Section } = require('./Section/Section');
 
 export class App extends Component {
   state = {
@@ -9,21 +10,42 @@ export class App extends Component {
     bad: 0,
   };
 
-  handleFeedback = type => {
+  handleFeedback = option => {
     this.setState(prevState => ({
-      [type]: prevState[type] + 1,
+      [option]: prevState[option] + 1,
     }));
   };
 
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const total = this.countTotalFeedback();
+    return total === 0 ? 0 : Math.round((this.state.good / total) * 100);
+  };
+
   render() {
+    const { good, neutral, bad } = this.state;
+    const total = this.countTotalFeedback();
+    const positivePercentage = this.countPositiveFeedbackPercentage();
+
     return (
       <div>
-        <Feedback onLeaveFeedback={this.handleFeedback} />
-        <Statistics
-          good={this.state.good}
-          neutral={this.state.neutral}
-          bad={this.state.bad}
-        />
+        <Section title="Please leave feedback">
+          <Feedback onLeaveFeedback={this.handleFeedback} />
+        </Section>
+
+        <Section title="Statistics">
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={positivePercentage}
+          />
+        </Section>
       </div>
     );
   }
